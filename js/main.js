09bd1d24cc36a2c9dd744b03dcc24da3471e10d9
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-01-14 22:57:54 +0530

    css

--- js/main.js
+++ js/main.js
@@ -1,44 +1,91 @@
-function table(a){
-    TL = [
-          ['ADC','DSP','MFE','DSP','LIC','CS'],
-          ['LIC','DSP','DM','AIC Lab','AIC Lab','AIC Lab'],
-          ['MFE','LIC','Minor','DSP Lab','DSP Lab','DSP Lab'],
-          ['ADC','Minor','CS','ADC','DM','MFE'],
-          ['ADC','CS','Minor','CS','DSP','LIC']
+  
+$ = (a) => document.querySelector(a);
+function set(today,time,j,i){
+  k='sub'
+  if(today[i]=='BREAK'){
+     k='break'
+  }
+  else if(today[i]=='LUNCH BREAK'){
+     k='lbreak'
+  }
+  return  `<div class='child ${k}'>
+              <div class="sub">
+                  <p class='per'>${k=='sub' ? j : ''}</p>            
+                  ${today[i]}
+                  <div class="time">
+                      ${time[i]}
+                  </div>
+              </div>
+            </div>`
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function gen(a){
+
+    let TL = [
+          ['ADC','DSP','BREAK','MFE','LUNCH BREAK','DSP','LIC','BREAK','CS'],
+          ['LIC','DSP','BREAK','DM','LUNCH BREAK','AIC Lab','AIC Lab','AIC Lab'],
+          ['MFE','LIC','BREAK','Minor','LUNCH BREAK','DSP Lab','DSP Lab','DSP Lab'],
+          ['ADC','Minor','BREAK','CS','LUNCH BREAK','ADC','DM','BREAK','MFE'],
+          ['ADC','CS','BREAK','Minor','LUNCH BREAK','CS','DSP','BREAK','LIC']
          ]
-k = `<tr>
-    <td>1</td>
-    <td>2</td>
-    <td>Break</td>
-    <td>3</td>
-    <td>Lunch</td>
-    <td>Lunch</td>
-    <td>4</td>
-    <td>5</td>
-    <td>Break</td>
-    <td>6</td>
-</tr>
-<tr>
-    <td>9 AM to 9:50 AM</td>
-    <td>9:50 AM to 10:40 AM</td>
-    <td>10:40 AM to 11 AM</td>
-    <td>11 AM to 11:50 AM<td>
-    <td>11:50 AM to 1 PM</td>
-    <td>1 PM to 1:50</td>
-    <td>1.50 PM to 2:40 PM</td>
-    <td>2:40 PM to 3 PM </td>
-    <td>3 PM to 4 PM</td>
-</tr>
-<tr>
-    <td>${TL[a][0]}</td>
-    <td>${TL[a][1]}</td>
-    <td></td>
-    <td>${TL[a][2]}<td>
-    <td></td>
-    <td>${TL[a][3]}</td>
-    <td>${TL[a][4]}</td>
-    <td></td>
-    <td>${TL[a][5]}</td>
-</tr>`
-document.querySelector('table').innerHTML = k
+    let time =  [
+                  [
+                    '&nbsp; 9 AM to 9:50 AM &nbsp;',
+                    '9:50 AM to 10:40 AM',
+                    '10:40 AM to 11 AM', 
+                    ' 11 AM to 11:50 AM',
+                    ' 11:50 AM to 1 PM  ', 
+                    ' 1 PM to 1:50 PM   ', 
+                    ' 1.50 PM to 2:40 PM', 
+                    '  2:40 PM to 3 PM  ',
+                    '    3 PM to 4 PM   '
+                  ],
+                  [
+                    '9 AM to 9:50 AM',
+                    '9:50 AM to 10:40 AM',
+                    '10:40 AM to 11 AM', 
+                    '11 AM to 11:50 AM',
+                    '11:50 AM to 1 PM', 
+                    '1 PM to 1:50 PM', 
+                    '1.50 PM to 2:40 PM', 
+                    '2:40 PM to 3 PM',
+                    '3 PM to 4 PM'
+                 ]
+              ]
+
+    let date = {
+                0:'Mon',
+                1:'Tue',
+                2:'Web',
+                3:'Thu',
+                4:'Fri',
+                5:'Sat',
+                6:'Sun'
+              }
+    if(a=='auto'){
+      today = new Date().toString().split(' ')[0];
+      for(i=0;i<5;i++)
+      if(date[i]==today)
+      today = TL[i]
+    }
+    else{
+      today = TL[a]
+    }
+    
+    today == 'Fri' ? time = time[1] : time = time[0]
+    
+    j=1
+   
+    $('.father').innerHTML=''
+    for(i=0;i<today.length;i++){
+        $('.father').innerHTML +=set(today,time,j,i)
+        await delay(98)
+        if(today[i]!='BREAK' && today[i]!='LUNCH BREAK')
+        j++
+    }
+    
 }
